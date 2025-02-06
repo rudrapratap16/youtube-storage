989c9e85b2d67b0297cfa2a4d874a8676763b86c
rudrapratap16 <145485930+rudrapratap16@users.noreply.github.com>
2025-02-07 00:57:28 +0530

    Delete purge.py

--- purge.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for file in os.listdir('./download and decode/temporary/'):
-    os.remove(f'./download and decode/temporary/{file}')
-for file in os.listdir('./upload to youtube/temporary/'):
-    os.remove(f'./upload to youtube/temporary/{file}')
