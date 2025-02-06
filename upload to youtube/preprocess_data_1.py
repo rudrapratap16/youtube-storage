4116ce4d27c08dedd0d039ee6d73ac20ffff3f29
rudrapratap16 <145485930+rudrapratap16@users.noreply.github.com>
2025-02-06 23:27:19 +0530

    Updating logic for file uploads

--- upload to youtube/preprocess_data_1.py
+++ upload to youtube/preprocess_data_1.py
@@ -1,36 +1,75 @@
 import pandas as pd
 import math
 import numpy as np
+import qrcode
+import cv2
+import base64
 
-path_to_csv_file = r'path_to_csv_file'
+path_to_csv_file = "Nifty 50_ONE_DAY.csv"
 
-# Converting CSV to String for making QR easily
-df = pd.read_csv(path_to_csv_file)
-data_row = ''
-for row in df.itertuples():
-    data_row += str(list(row))
+def make_qr(ele):
+    qr = qrcode.QRCode(
+    version=10,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+    )
+    qr.add_data(data=ele)
+    qr.make(fit=True)
 
-# Breaking string into lists of length 2000
-data_array = []
-data_length = 2000
-temp = 0
-for index, letter in enumerate(data_row):
-    if index%data_length == 0 and index != 0:
-        data_array.append(data_row[temp:index])
-        temp = index
-data_array.append(data_row[temp:])
+    img = qr.make_image(fill_color="black", back_color="white")
+    
+    # Get image in PIL format
+    pil_img = img.get_image()
 
-# Logic for counting words
-counter = 0
-for arr in data_array:
-    for word in arr:
-        counter += 1
+    # Converting to cv2 image and resizing it
+    cv_img = pil_img.convert('RGB')
+    image = np.array(cv_img)
+    image = cv2.resize(image, (1080, 1080))
+    return image
 
-np.save('./temporary/data_array.npy', data_array)
+chunk_size = 1000 # 1 KiB
+qr_images = []
+data = 0
+with open(path_to_csv_file, "rb") as in_file:
+    while True:
+        chunk = in_file.read(chunk_size)
+            
+        if chunk == b"":
+            break # end of file
+        chunk = base64.b64encode(chunk).decode('utf-8')
+        qr = make_qr(chunk)
+        qr_images.append(qr)
 
-print(f'Total QR codes : {len(data_array)}')    
-print(f'Total time of video : { len(data_array)/30 if len(data_array)/30 < 1 else math.ceil(len(data_array)/30)}')
-print(f'Total words : {counter}')
-print(f'Estimated size for total QR codes : {(len(data_array)*475)//1000}')
+print("\n\n------Done with making arrays------\n\n")
 
-print('\n\n------Done with Preprocessing step------')
+# Video Creation Part   
+start_buffer_folder = './buffers/start'
+end_buffer_folder = './buffers/end'
+video_name = './temporary/video.avi'
+
+# Creating array by joining values of each frame
+images = []
+for i in range(60):
+    images.append(cv2.imread(f'{start_buffer_folder}/start_buffer_0.png'))
+images.extend(qr_images)
+for i in range(60):
+    images.append(cv2.imread(f'{end_buffer_folder}/end_buffer_0.png'))
+
+frame = images[0]
+height, width, layers = frame.shape
+
+# Initializing video writer and setting it to 30 fps
+video = cv2.VideoWriter(video_name, 0, 30, (width,height))
+
+print("Processing Video...")
+
+# Joining images frame by frame to create video
+for image in images:
+    video.write(image)
+
+cv2.destroyAllWindows()
+video.release()
+
+print(f"Made video at location : {video_name}")
+print("\n\n------Done with making video using arrays------\n\n")
